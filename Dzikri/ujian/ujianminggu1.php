bcb0496e53b1df280c7034aa68a4adfe88204b7f
adzri27 <adzri2705@@gmail.com>
2024-08-10 17:37:19 +0700

    sabtu-10-8-2024

---
+++ Dzikri/ujian/ujianminggu1.php
@@ -0,0 +1,31 @@
+<?php
+$nama = "adzri";
+
+echo $nama;
+?>
+
+<?php
+
+$x = 10;
+$x += 20;
+
+echo $x ;
+?>
+
+<?php
+
+$nama1 = "apa";
+$nama2 = "aja";
+
+echo $nama1 ." ".$nama2;
+?>
+
+<?php
+var_dump(5 === "5")
+?>
+
+<?php
+$x = "10";
+var_dump( $x < 20; &&  )
+
+?>
