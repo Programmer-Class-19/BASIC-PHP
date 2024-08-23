8e2a3f3de9362b3402a21349e427503405586915
adzri27 <adzri2705@@gmail.com>
2024-08-23 21:49:38 +0700

    jum'at,23-08-2024

---
+++ Dzikri/Sprint3/OOP PHP/latihan oop/katihan2.php
@@ -0,0 +1,67 @@
+<?php
+
+class produk {
+    public $judul,
+           $penulis,
+           $penerbit,
+           $harga,
+           $jmlhalaman,
+           $waktumain,
+           $tipe;
+
+    public function __construct($judul, $penulis, $penerbit, $harga, $jmlhalaman, $waktumain, $tipe) {
+        $this->judul = $judul;
+        $this->penulis =$penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+        $this->jmlhalaman = $jmlhalaman;
+        $this->waktumain = $waktumain;
+        $this->tipe = $tipe;
+    }
+
+    public function getLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
+
+    public function getInfoProduk() {
+        $str = "{$this->tipe} : {$this->getLabel()} | (Rp.{$this->harga})";
+
+        // if ($this->tipe = "Komik") {
+        //     $str .= "- {$this->jmlhalaman} Halaman.";
+        // }elseif ($this->tipe = "Game") {
+        //     $str .= "- {$this->waktumain} Game.";
+        // }
+
+        // return $str;
+    }
+}
+
+class Komik extends Produk {
+    public function getInfoProduk() {
+        $str = "Komik : {$this->judul} | {$this->getLabel()} (Rp.{$this->harga} - {$this->jmlhalaman}) Halaman.";
+        return $str;
+    }
+}
+
+class Game extends Produk {
+    public function getInfoProduk() {
+        $str = "Game : {$this->judul} | {$this->getLabel()} (Rp.{$this->harga}) ~ {$this->waktumain} Jam.";
+
+        return $str;
+    }
+}
+
+class cetakInfoProduk {
+    public function cetak(produk $produk){
+        $str = "{$produk->produk} | {$produk->getLabel()} (Rp.{$this->harga})";
+
+        return $str;
+    }
+}
+
+$produk1 = new Komik("Naruto", " Masashi kishimoto", "Shounen Jump", 150000, 100, 0, "Komik");
+$produk2= new Game("Mobile legend", "Moonton", "Moonton", 300000, 0, 50, "Game");
+
+echo $produk1->getinfoProduk();
+echo "<br>";
+echo $produk2->getinfoProduk();
---
+++ Dzikri/Sprint3/OOP PHP/visibility.php
@@ -0,0 +1,75 @@
+<?php
+
+class produk {
+    public $judul,
+           $penulis,
+           $penerbit,
+          
+     $harga;
+           
+           
+        
+
+    public function __construct($judul, $penulis, $penerbit, $harga = 0) {
+        $this->judul = $judul;
+        $this->penulis =$penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    public function getLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
+
+    public function getInfoProduk() {
+        $str = "{$this->judul} | {$this->getLabel()}  (Rp.{$this->harga})";
+        return $str;
+    }
+}
+
+class Komik extends Produk {
+    public $jmlhalaman;
+
+    public function __constract( $judul, $penulis, $penerbit, $harga, $jmlhalaman = 0 ) {
+        parent::__constract($judul, $penulis, $penerbit, $harga = 0);
+
+        $this->jmlhalaman = $jmlhalaman;
+    }
+
+    public function getInfoProduk() {
+        $str = "Komik : " . parent::getInfoProduk() . "- {$this->jmlhalaman} Halaman.";
+        return $str;
+    }
+}
+
+class Game extends Produk {
+    public $waktumain;
+
+    public function __constract($judul, $penulis, $penerbit, $harga = 0 ) {
+        parent::__constract($judul, $penulis, $penerbit, $harga, $waktumain = 0) ;
+
+        $this->waktumain = $waktumain;
+    }
+    public function getInfoProduk() {
+        $str = "Game : " . parent::getInfoProduk() . " ~ {$this->waktumain} Jam.";
+
+        return $str;
+    }
+}
+
+class cetakInfoProduk {
+    public function cetak(produk $produk){
+        $str = "{$produk->produk} | {$produk->getLabel()} (Rp.{$this->harga})";
+
+        return $str;
+    }
+}
+
+$produk1 = new Komik("Naruto", " Masashi kishimoto", "Shounen Jump", 150000, 100, "Komik");
+$produk2= new Game("Mobile legend", "Moonton", "Moonton", 300000, 50, "Game");
+
+echo $produk1->getinfoProduk();
+echo "<br>";
+echo $produk2->getinfoProduk();
+echo "<hr>";
+echo $produk1->harga;
