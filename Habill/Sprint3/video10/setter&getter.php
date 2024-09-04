56ec66cba34c0ea488c4b4d5c060c4fd0b6b17b1
qibiell <kobilbryant5@gmail.com>
2024-09-04 23:22:43 +0700

    nambah file

--- Habill/Sprint3/video10/setter&getter.php
+++ Habill/Sprint3/video10/setter&getter.php
@@ -4,11 +4,9 @@ class produk {
     private 
     $judul,
     $penulis,
-    $penerbit;
-
-    protected $diskon = 0;
-
-    private $harga;  
+    $penerbit,
+    $harga,
+    $diskon = 0; 
 
     public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0) {
         $this->judul = $judul;
@@ -17,8 +15,43 @@ class produk {
         $this->harga = $harga;
     }
 
-    public function getJudul() {
-        return $this->Judul;
+    public function setJudul($judul) {
+        // if (!is_string($judul) ) {
+        //     throw new exception("error blok");
+        // }
+        $this->judul = $judul;
+    }
+
+    public function GetJudul() {
+        return $this->judul;
+    }
+
+    public function setPenulis($penulis) {
+        $this->penulis = $penulis;
+    }
+
+    public function getPenulis() {
+        return $this->penulis;
+    }
+
+    public function setPenerbit($penerbit) {
+        $this->penerbit = $penerbit;
+    }
+
+    public function GetPenerbit() {
+        return $this->penerbit;
+    }
+
+    public function setDiskon($diskon) {
+        $this->diskon = $diskon;
+    }
+
+    public function getDikson() {
+        return $this->diskon;
+    }
+
+    public function setHarga($harga) {
+        $this->harga = $harga;
     }
 
     public function getHarga(){
@@ -62,11 +95,6 @@ class game extends produk {
         $this->waktuMain = $waktuMain;
     }
 
-    public function setDiskon($diskon) {
-        $this->diskon = $diskon;
-    }
-    
-
     public function getInfoProduk() {
         $str = "game :  " . parent::getInfoProduk() . " - {$this->waktuMain} jam.";
         return $str;
@@ -95,10 +123,18 @@ $produk2->setDIskon(50);
 echo $produk2->getHarga();
 echo "<hr>";
 
-echo $produk1->getJudul();
+$produk1->setPenulis("fuckyouu");
+echo $produk1->GetPenulis();
+
+// $produk1->setJudul("friendzone");
+// echo $produk1->getJudul();
 
 // $produk3 = new produk("barang baru");
 // echo $produk1->judul; // bakalan error
 
+
+
+// overloading :
+// __set() & __get()
 
 ?>
---
+++ Habill/Sprint3/video11/statickeywoard.php
@@ -0,0 +1,22 @@
+<?php 
+
+// class merupakan template dari object
+
+// kita bisa mengakses property dan method dalam konteks class
+
+class contohStatic {
+    public static $angka = 1;
+
+    public static function halo() {
+        return "hallo";
+    }
+}
+
+
+
+
+
+
+
+
+?>
