b010e6affccc35630ef91c4496b3031fb00551e1
syauqie13 <syauqiebilah13@gmail.com>
2024-09-10 10:13:10 +0700

    lagi pliss

--- Syauqie/Sprint4/3php&mysql/index2.php
+++ Syauqie/Sprint4/3php&mysql/index2.php
@@ -47,3 +47,14 @@ $mahasiswa = query("SELECT * FROM mahasiswa")
     </table>
 </body>
 </html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
