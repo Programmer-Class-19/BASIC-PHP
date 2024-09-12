873ef91c7346f52460e795bcd7e6ed7479870489
syauqie13 <syauqiebilah13@gmail.com>
2024-09-12 20:46:51 +0700

    12 september 2024

---
+++ Syauqie/Sprint4/phpmvc/6.assets/app/init.php
@@ -0,0 +1,6 @@
+<?php 
+    require_once 'core/App.php'; //menggunakan kapital untuk menandakan ini sebuah klass
+    require_once 'core/Controller.php'; //2 klass ini adalah yang utama atas terbentuknya MVC
+
+    require_once 'core/Constants.php'; //berisi css
+?>
