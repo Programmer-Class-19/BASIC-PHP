3f29929f03372f080bbe147ad3dbb5cd4698ccee
syauqie13 <syauqiebilah13@gmail.com>
2024-09-15 23:44:21 +0700

    15 september 2024

---
+++ Syauqie/Sprint4/phpmvc/11.deletedata/app/config/config.php
@@ -0,0 +1,10 @@
+<?php 
+    define('BASEURL', 'http://localhost/PEMBELAJARANIT/BASIC-PHP/Syauqie/Sprint4/phpmvc/11.deletedata/public');
+
+    //DB
+
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'phpmvc');
+?>
