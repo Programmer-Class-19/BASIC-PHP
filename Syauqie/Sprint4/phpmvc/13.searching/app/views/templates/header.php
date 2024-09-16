07130bfd8232bf2c0d53b7692e5a8b8171dbe41e
syauqie13 <syauqiebilah13@gmail.com>
2024-09-16 22:07:40 +0700

    16 september 2024

---
+++ Syauqie/Sprint4/phpmvc/13.searching/app/views/templates/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman <?= $data['judul'] ?></title>
+    <link rel="stylesheet" href="<?= BASEURL; ?>/css/bootstrap.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> <!-- jQuery CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> <!-- SweetAlert CDN -->
+    <script> const baseURL = '<?= BASEURL; ?>';</script>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">  
+        <div class="container">
+            <a class="navbar-brand" href="<?= BASEURL; ?>">PHP MVC</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="<?= BASEURL; ?>">Home</a>
+                    <a class="nav-link" href="<?= BASEURL; ?>/mahasiswa">Mahasiswa</a>
+                    <a class="nav-link" href="<?= BASEURL; ?>/about">About</a>
+                </div>
+            </div>
+        </div>
+    </nav>
